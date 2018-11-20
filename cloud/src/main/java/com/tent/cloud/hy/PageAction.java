4f7fe1c851ab84878a7a3e872e050531403358c7
haobingfu <haobingfu@frogsing.com>
2018-11-20 09:51:20 +0800

    modify commit

---
+++ cloud/src/main/java/com/tent/cloud/hy/PageAction.java
@@ -0,0 +1,16 @@
+//package com.tent.cloud.hy;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//@RequestMapping(value = "/")
+//public class PageAction {
+//
+//    @RequestMapping(value = "index.html")
+//    public String index(Model model){
+//        model.addAttribute("msg","你好spring mvc");
+//        return "index";
+//    }
+//}
