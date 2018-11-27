77de8fa6b84fb5befd5b154bc42cd5136b363e58
haobingfu <haobingfu@frogsing.com>
2018-11-27 11:55:38 +0800

    modify commit

--- cloud/src/main/java/com/tent/cloud/hy/PageAction.java
+++ cloud/src/main/java/com/tent/cloud/hy/PageAction.java
@@ -1,16 +1,17 @@
-//package com.tent.cloud.hy;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//@RequestMapping(value = "/")
-//public class PageAction {
-//
-//    @RequestMapping(value = "index.html")
-//    public String index(Model model){
-//        model.addAttribute("msg","你好spring mvc");
-//        return "index";
-//    }
-//}
+package com.tent.cloud.hy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageAction {
+
+    @RequestMapping(value = "/index.html")
+    public String index(Model model){
+        model.addAttribute("msg","你好spring mvc");
+        System.out.println("jinrul _________====");
+        return "index";
+    }
+
+}
