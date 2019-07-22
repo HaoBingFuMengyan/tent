2ae63f38b106cfad552674003eb5fe88e5c16edf
haobingfu <haobingfu@frogsing.com>
2019-07-22 11:42:22 +0800

    commit modify

--- cloud/src/main/java/com/tent/cloud/shiro/filter/ShiroFormAuthenticationFilter.java
+++
@@ -1,8 +0,0 @@
-package com.tent.cloud.shiro.filter;
-
-import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
-
-public class ShiroFormAuthenticationFilter extends FormAuthenticationFilter {
-
-
-}
